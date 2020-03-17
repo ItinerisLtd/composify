1b7090d40561bcacb8eb3ceba20acb512478fcd8
TangRufus <tangrufus@gmail.com>
2020-03-17 12:37:13 +0000

    yarn add chalk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {Command, flags} from '@oclif/command'
-import chalk from 'chalk'
+import * as chalk from 'chalk'
 import * as execa from 'execa'
 import * as fs from 'fs-extra'
 import * as tmp from 'tmp'
